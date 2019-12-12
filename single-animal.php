92c87236a55e5d575132f95e5a5fdafec59049ae
Isaque Melo <isaquegbmelo2@gmail.com>
2019-12-11 22:01:20 -0300

    adiciona fotos animal

--- single-animal.php
+++ single-animal.php
@@ -12,8 +12,12 @@ get_header();
 		<div class="col-md-9 content-holder">
       <div class="animal-header">
   			<div class="content-title col-md-3 no-padding">
-  				<span><?php the_field('nome'); ?></span>
+  				<span class=""><?php the_field('nome'); ?></span>
           <span class="animal-id">#<?php echo get_the_ID(); ?></span>
+          <a href="#" class="nova-consulta-btn default-btn col-md-11">
+            Nova consulta
+          </a>
+
   			</div>
 
         <div class="cota-mensal col-md-9 no-padding">
@@ -35,16 +39,65 @@ get_header();
         </div>
       </div>
 
-			<div class="list">
-				<!-- <div class="list-header col-md-12">
-						<div class="col-md-1">Cód.</div>
-						<div class="col-md-3">Nome</div>
-						<div class="col-md-6">Nome do dono</div>
-						<div class="col-md-1">Plano</div>
-				</div> -->
+			<div class="animal-dados">
+			     <div class="animal-data">
+             Dados
+           </div>
+
+           <div class="dados-container">
+             <div class="animal-nome">
+               Nome: <span class="bold-font"><?php the_field('nome'); ?></span>
+             </div>
+
+             <div class="animal-plano">
+               Plano: <span class="bold-font"><?php the_field('plano'); ?></span>
+             </div>
+
+             <div class="animal-dono">
+               Nome do dono: <span class="bold-font"><?php the_field('nome_do_dono'); ?></span>
+             </div>
+
+             <div class="animal-cpf">
+               CPF: <span class="bold-font"><?php the_field('cpf_dono'); ?></span>
+             </div>
+
+             <div class="animal-end">
+               <?php
+                $end = array('rua' => get_field('rua'),
+                             'cidade' => get_field('cidade'),
+                             'estado' => get_field('estado')
+                );
+
+               ?>
+               Endereço: <span class="bold-font"><?php echo "{$end['rua']}". ", ". "{$end['cidade']} - {$end['estado']}"; ?></span>
+             </div>
+
+             <div class="animal-telefone">
+               Telefone: <span class="bold-font"><?php the_field('telefone'); ?></span>
+             </div>
+
+             <div class="animal-celular">
+               Celular: <span class="bold-font"><?php the_field('celular'); ?></span>
+             </div>
+
+           </div>
 
 
 
+
+
+           <?php
+           $images = get_field('imagens');
+           $size = 'full'; // (thumbnail, medium, large, full or custom size)
+           if( $images ): ?>
+               <ul>
+                   <?php foreach( $images as $image_id ): ?>
+
+                           <img src="<?php echo $image_id; ?>" alt="" width="150">
+
+                   <?php endforeach; ?>
+               </ul>
+           <?php endif; ?>
 
 
 			</div>
