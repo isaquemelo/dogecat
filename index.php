08bf0a73bcc15b38b6fb7199e1e4910264e78d46
Isaque Melo <isaquegbmelo2@gmail.com>
2019-12-26 17:30:15 -0300

    adiciona pagina de inicio

--- index.php
+++ index.php
@@ -2,28 +2,142 @@
 
 
 
-if (!is_user_logged_in()) {
+
+if (!is_user_logged_in()){
 	auth_redirect();
 }
 
+$requested = false;
 
+function a_post_exists($id) {
+	return is_string( get_post_status( $id ) );
+}
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+	if(isset($_POST['submitted']) && isset($_POST['post_nonce_field']) && wp_verify_nonce($_POST['post_nonce_field'], 'post_nonce')) {
+		$animal_id = $_POST['cod'];
+		if (a_post_exists($animal_id)) {
+			$post_type = get_post_type($animal_id);
+			if (strcmp($post_type, "animal") == 0) {
+				$post = get_post($animal_id);
+				wp_redirect(home_url() . "/animal/" . $animal_id);
+			} else {
+				echo '<script language="javascript">';
+				echo 'alert("Animal não encontrado.")';
+				echo '</script>';
+				$requested = null;
+			}
+
+		} else {
+			$requested = null;
+			echo '<script language="javascript">';
+			echo 'alert("Animal não encontrado.")';
+			echo '</script>';
+		}
+	}
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && $_GET['nova_consulta']) {
+	MB_Relationships_API::add( $_GET['animal_id'], $_GET['consulta_id'], 'animal_to_consulta' );
+	wp_redirect(home_url() . "/animal/" . $_GET['animal_id']);
+}
+
+acf_form_head();
 get_header();
+
 
 ?>
 
 <div id="primary" class="home-wrapper col-md-12">
 		<?php get_sidebar(); ?>
 		<div class="col-md-9 content-holder">
+			<div class="col-md-5 no-padding">
+				<div class="content-title col-md-12 no-padding">
+					<span>Início</span>
+				</div>
 
-			<div class="content-title">
-				<span>Início</span>
+				<div class="buttons col-md-12 no-padding">
+					<a href="<?php echo home_url() . '/cadastrar-animal'; ?> " class="novo-animal">Cadastrar novo animal</a>
+					<a href="<?php echo home_url() . '/cadastrar-veterinario'; ?>" class="nova-consulta">Nova consulta</a>
+				</div>
+
+				<div class="ir-animal col-md-12 no-padding">
+					<form action="" method="post">
+						<input type="text" placeholder="Código do animal" name="cod">
+						<input type="submit" value="Ir">
+						<?php wp_nonce_field('post_nonce', 'post_nonce_field'); ?>
+						<input type="hidden" name="submitted" id="submitted" value="true">
+					</form>
+				</div>
+
+
+				<div class="col-md-12 no-padding content-holder">
+
+					<div class="content-title">
+						<span>Animais</span>
+					</div>
+
+					<div class="list">
+						<div class="list-header col-md-12 no-padding">
+								<div class="col-md-4 no-padding">Cód.</div>
+								<div class="col-md-4 no-padding">Nome</div>
+								<div class="col-md-3 no-padding">Plano</div>
+						</div>
+
+						<?php
+							$temp = $wp_query;
+							$wp_query = null;
+							$wp_query = new WP_Query();
+							$wp_query->query('showposts=20&post_type=animal'.'&paged='.$paged);
+
+						?>
+
+
+						<?php while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
+							<div class="list-data col-md-12 no-padding">
+									<a href="<?php the_permalink() ?>">
+										<div class="col-md-4 no-padding">#<?php echo get_the_ID(); ?></div>
+										<div class="col-md-4 no-padding"><?php the_field('nome'); ?></div>
+										<div class="col-md-3 no-padding" ><?php the_field('plano'); ?></div>
+									</a>
+							</div>
+
+						<?php endwhile; ?>
+
+						<?php odin_paging_nav() ?>
+
+
+
+					</div>
+
+				</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 			</div>
+
+
+
+
 
 
 
 		</div>
 
-	</div><!-- #primary END -->
+</div><!-- #primary END -->
 
 
 <?php get_footer(); ?>
