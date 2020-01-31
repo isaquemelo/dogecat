0cbcf436f91350952a9d12e819011d1e2f404ab9
Isaque Melo <isaquegbmelo2@gmail.com>
2020-01-31 20:53:39 -0300

    coloca cor preta

--- template-carteirinha.php
+++ template-carteirinha.php
@@ -207,8 +207,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 			<div class="content">
 				Dúvidas na escolha do veterinário? Temos uma lista dos mais elogiados pelos nosso próprios clientes. Ligue-nos:
 				(083) 3201-5966  /
-				(083) 9 8720-7140 /
-				(083) 9 9827-3543
+				(083) 9 8720-7140
 
 			</div>
 
